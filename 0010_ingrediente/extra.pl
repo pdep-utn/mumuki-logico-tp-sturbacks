4a51119dfe17927864afdfe05ddf1f7ab6ab9b0a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-03 16:48:09 -0300

    Eliminando código innecesario

--- 0010_ingrediente/extra.pl
+++ 0010_ingrediente/extra.pl
@@ -1,21 +1,3 @@
-%%%%%% Base de conocimiento %%%%
-pedido(vero,frappuccinoFrutilla).
-pedido(vero, light).
-pedido(gus,irishCream).
-pedido(gus,explosiva).
-pedido(gus,extrema).
-pedido(gus,frappuccinoFrutilla).
-pedido(alf,conTodo).
-pedido(franco,light).
-pedido(franco,frappuccinoFrutilla).
-pedido(franco,irishCream).
-pedido(franco,explosiva).
-pedido(franco,extrema).
-pedido(franco,conTodo).
-pedido(nico, frappuccinoFrutilla).
-pedido(nico, dulceDeLecheLatte).
-pedido(nico, light).
-
 bebida(dulceDeLecheLatte,[base(cafe,100),leche(10,50)]).
 bebida(frappuccinoFrutilla,[base(helado,80),jarabe(frutilla),jarabe(dulceDeLeche),leche(2,60)]).
 bebida(irishCream,[base(cafe,90),jarabe(baileys),leche(3,50)]).
@@ -27,8 +9,3 @@ bebida(cafecito,[base(cafe,10)]).
 bebida(conTodo,[base(cafe,5),jarabe(frutilla),base(helado,80),base(mouse,90),leche(10,10),jarabe(chocolate),base(helado,80),base(mouse,90),leche(10,10),jarabe(chocolate),base(helado,80),base(mouse,90),leche(10,10),jarabe(chocolate)]).
 bebida(milkshake, [base(helado, 80), leche(5, 10)]).
 bebida(cafeConLeche, [base(cafe, 5), leche(5, 10)]).
-
-tieneAlcohol(baileys).
-tieneAlcohol(tiaMaria).
-tieneAlcohol(vodka).
-tieneAlcohol(ron).
